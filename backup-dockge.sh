318c60cad6e322b9702e09d550f2620ff3eb7285
Rambo <ramborigs90@gmail.com>
2024-11-24 21:45:14 +0000

    More echo

--- backup-dockge.sh
+++ backup-dockge.sh
@@ -25,6 +25,7 @@ for dir in "$STACKS_DIR"/*; do
     if [ -d "$dir" ]; then
         # Get directory name without path
         dir_name=$(basename "$dir")
+        echo $dir_name
         mkdir -p "$BACKUP_DIR/$dir_name"
         # If the directory is "fireshare", exclude the "videos" folder from the archive
         if [ "$dir_name" == "fireshare" ]; then
